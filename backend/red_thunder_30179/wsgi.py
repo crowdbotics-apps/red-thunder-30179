89a57eb0cf06edc869e5e3935109e26f95b90b38
Crowdbotics <team@crowdbotics.com>
2021-08-29 13:24:49 +0000

    Initial commit.

---
+++ backend/red_thunder_30179/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for red_thunder_30179 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'red_thunder_30179.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "red_thunder_30179Identifier",
+  url: "https://red_thunder_30179Identifier.botics.co",
+  api: "https://red_thunder_30179Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
